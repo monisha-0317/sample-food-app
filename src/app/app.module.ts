e2be56d870b2cd946bde841069cb16184f823242
NaveenGanesh241 <naveenganesh241@gmail.com>
2023-05-12 15:04:38 +0530

    button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
 import { FoodPageComponent } from './food-page/food-page.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import { FoodPageComponent } from './food-page/food-page.component';
     HomeComponent,
     SearchComponent,
     TagsComponent,
-    FoodPageComponent
+    FoodPageComponent,
+    ButtonsComponent,
+   
   ],
   imports: [
     BrowserModule,
--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class FoodPageComponent {
 
+  buttonval?:"Go To Cart"
 }
